b17a2bc73bdb004eb522d2b9e20ab6a67666557e
zzc6332 <zzc6332@foxmail.com>
2026-03-07 18:08:00 +0800

    feat: initial stage

--- src/d-path/index.ts
+++ src/d-path/index.ts
@@ -1,232 +1,24 @@
 import type {
-  ContraFilterTurple,
-  PrependParams,
-  TupleToIntersection,
-} from "./utility-types";
+  Coord,
+  AbsoluteCommand,
+  Command,
+  PathSegmentArgs,
+  CreatePathSegmentInfoOverloads,
+  CreatePathOverloads,
+  pathSegmentInfo,
+  ToAbsoluteCommand,
+  AddPathOverloads,
+} from "./types";
 
-type RelativeCommand = "l" | "h" | "v" | "c" | "s" | "q" | "t" | "a";
-type AbsoluteCommand = "L" | "H" | "V" | "C" | "S" | "Q" | "T" | "A";
-type Command = RelativeCommand | AbsoluteCommand;
-type ToAbsoluteCommand<Command> = Command extends "l" | "L"
-  ? "L"
-  : Command extends "h" | "H"
-    ? "H"
-    : Command extends "v" | "V"
-      ? "V"
-      : Command extends "c" | "C"
-        ? "C"
-        : Command extends "s" | "S"
-          ? "S"
-          : Command extends "q" | "Q"
-            ? "Q"
-            : Command extends "t" | "T"
-              ? "T"
-              : Command extends "a" | "A"
-                ? "A"
-                : never;
-type toCommonCommand<AbsoluteCommand> = AbsoluteCommand extends "L"
-  ? "l" | "L"
-  : AbsoluteCommand extends "H"
-    ? "h" | "H"
-    : AbsoluteCommand extends "V"
-      ? "v" | "V"
-      : AbsoluteCommand extends "C"
-        ? "c" | "C"
-        : AbsoluteCommand extends "S"
-          ? "s" | "S"
-          : AbsoluteCommand extends "Q"
-            ? "q" | "Q"
-            : AbsoluteCommand extends "T"
-              ? "t" | "T"
-              : AbsoluteCommand extends "A"
-                ? "a" | "A"
-                : never;
-
-type Coord = [number, number];
-
-// 用于 createPath 方法的重载签名组成的元祖类型，后续可以转为交叉类型来实现重载；其它方法有类似的重载签名也可以以这个元祖为基础映射类型以进行拓展
-type AddPathOverloadsTuple = [
-  <C extends "l" | "L">(command: C, endX: number, endY: number) => PathData,
-  <C extends "l" | "L">(command: C, end: Coord) => PathData,
-  <C extends "h" | "H">(command: C, endX: number) => PathData,
-  <C extends "v" | "V">(command: C, endY: number) => PathData,
-  <C extends "c" | "C">(
-    command: C,
-    controlPointX1: number,
-    controlPointY1: number,
-    controlPointX2: number,
-    controlPointY2: number,
-    endX: number,
-    endY: number,
-  ) => PathData,
-  <C extends "c" | "C">(
-    command: C,
-    controlPoint1: Coord,
-    controlPoint2: Coord,
-    end: Coord,
-  ) => PathData,
-  <C extends "s" | "S">(
-    command: C,
-    controlPointX: number,
-    controlPointY: number,
-    endX: number,
-    endY: number,
-  ) => PathData,
-  <C extends "s" | "S">(command: C, controlPoint: Coord, end: Coord) => any,
-  <C extends "q" | "Q">(
-    command: C,
-    controlPointX: number,
-    controlPointY: number,
-    endX: number,
-    endY: number,
-  ) => PathData,
-  <C extends "q" | "Q">(command: C, controlPoint: Coord, end: Coord) => any,
-  <C extends "t" | "T">(command: C, endX: number, endY: number) => any,
-  <C extends "t" | "T">(command: C, end: Coord) => any,
-  <C extends "a" | "A">(
-    command: C,
-    rx: number,
-    ry: number,
-    rotation: number,
-    largeArcFlag: boolean | 0 | 1,
-    sweepFlag: boolean | 0 | 1,
-    endX: number,
-    endY: number,
-  ) => PathData,
-  <C extends "a" | "A">(
-    command: C,
-    rx: number,
-    ry: number,
-    rotation: number,
-    largeArcFlag: boolean | 0 | 1,
-    sweepFlag: boolean | 0 | 1,
-    end: Coord,
-  ) => PathData,
-  <C extends "a" | "A">(
-    command: C,
-    radius: number | [number, number],
-    end: Coord,
-    rotation?: number,
-    largeArcFlag?: boolean | 0 | 1,
-    sweepFlag?: boolean | 0 | 1,
-  ) => PathData,
-];
-
-// 用于将 AddPathOverloadsTuple 转换为 CreatePathOverloadsTuple 的工厂类型，实质上是向 AddPathOverloadsTuple 中的每个函数签名的参数列表前添加一个 start: Coord 参数
-type CreatePathOverloadsTupleFactory<T extends ((...args: any[]) => any)[]> =
-  T extends [
-    infer First extends (...args: any) => any,
-    ...infer Rest extends ((...args: any) => any)[],
-  ]
-    ? [
-        PrependParams<First, [start: Coord]>,
-        ...CreatePathOverloadsTupleFactory<Rest>,
-      ]
-    : [];
-// 用于 addPath 方法的重载签名组成的元祖类型
-type CreatePathOverloadsTuple =
-  CreatePathOverloadsTupleFactory<AddPathOverloadsTuple>;
-
-// 把一个函数签名的返回值变成 pathSegmentInfo, I 指定了要选用的 Command 类型在该函数参数中的序号
-type MakePathSegmentInfoResult<
-  T extends (...args: any[]) => any,
-  I extends number,
-> = T extends (...args: any[]) => any
-  ? (
-      ...args: Parameters<T>
-    ) => pathSegmentInfo<ToAbsoluteCommand<Parameters<T>[I]>>
-  : never;
-
-// 把一个函数签名的返回值变成 PathSegment，第二个泛型 I 指定了要选用的 Command 类型在该函数参数中的序号
-type MakePathSegmentResult<
-  T extends (...args: any[]) => any,
-  I extends number,
-> = T extends (...args: any[]) => any
-  ? (...args: Parameters<T>) => PathSegment<ToAbsoluteCommand<Parameters<T>[I]>>
-  : never;
-
-// 把 CreatePathOverloadsTuple 中的函数签名转换为 CreatePathSegmentInfoOverloadsTuple 的工具函数，
-type CreatePathSegmentInfoOverloadsTupleFactory<
-  T extends ((...args: any[]) => any)[],
-> = T extends [
-  infer First extends (...args: any) => any,
-  ...infer Rest extends ((...args: any) => any)[],
-]
-  ? [
-      MakePathSegmentInfoResult<First, 1>,
-      ...CreatePathSegmentInfoOverloadsTupleFactory<Rest>,
-    ]
-  : [];
-
-// 用于 createPathSegmentInfo 方法的重载签名组成的元祖类型
-type CreatePathSegmentInfoOverloadsTuple =
-  CreatePathSegmentInfoOverloadsTupleFactory<CreatePathOverloadsTuple>;
-
-// 用于 createPath 方法的重载签名
-type CreatePathOverloads = TupleToIntersection<CreatePathOverloadsTuple>;
-// 用于 addPath 方法的重载签名
-type AddPathOverloads = TupleToIntersection<AddPathOverloadsTuple>;
-// 用于 CreatePathSegmentInfoOverloads 方法的重载签名
-type CreatePathSegmentInfoOverloads =
-  TupleToIntersection<CreatePathSegmentInfoOverloadsTuple>;
-
-// 根据 SVG 命令名称返回对应命令参数组成的元祖类型
-type PathSegmentArgs<C extends Command> = C extends "l" | "L"
-  ? [number, number]
-  : C extends "h" | "H"
-    ? [number]
-    : C extends "v" | "V"
-      ? [number]
-      : C extends "c" | "C"
-        ? [number, number, number, number, number, number]
-        : C extends "s" | "S"
-          ? [number, number, number, number]
-          : C extends "q" | "Q"
-            ? [number, number, number, number]
-            : C extends "t" | "T"
-              ? [number, number]
-              : C extends "a" | "A"
-                ? [number, number, number, 0 | 1, 0 | 1, number, number]
-                : never;
-
-class PathSegment<C extends AbsoluteCommand = AbsoluteCommand> {
-  constructor(
-    public readonly start: Coord,
-    public readonly command: C,
-    public readonly args: PathSegmentArgs<C>,
-  ) {}
-
-  public get end(): Coord {
-    switch (this.command) {
-      case "L":
-      case "T":
-        return [this.args[0], this.args[1]] as Coord;
-      case "H":
-        return [this.args[0], this.start[1]] as Coord;
-      case "V":
-        return [this.start[0], this.args[0]] as Coord;
-      case "C":
-        return [this.args[4], this.args[5]] as Coord;
-      case "S":
-      case "Q":
-        return [this.args[2], this.args[3]] as Coord;
-      case "A":
-        return [this.args[5], this.args[6]] as Coord;
-      default:
-        throw new Error(`Invalid command: ${this.command}`);
-    }
-  }
-
-  public getSegment() {
-    return this.command + " " + this.args.join(" ");
-  }
-
-  public toString(): string {
-    return `M ${this.start[0]} ${this.start[1]} ${this.getSegment()} Z`;
-  }
-}
-
-class DPathCommon {
+// DPathCommon 中定义一些通用方法
+abstract class DPathCommon {
+  /**
+   * 将一些创建路径的方法的传参输出为统一的格式，如果传入的是使用相对坐标的命令，则结果会被转换为绝对坐标的形式
+   * @param start 起始点坐标
+   * @param command 路径命令
+   * @param args 其余的参数，支持多种传参格式，最终会被整理为与原生 SVG 路径命令参数相同的格式
+   * @returns
+   */
   protected createPathSegmentInfo: CreatePathSegmentInfoOverloads = <
     C extends Command,
   >(
@@ -330,10 +122,107 @@ class DPathCommon {
   };
 }
 
-interface pathSegmentInfo<C extends AbsoluteCommand = AbsoluteCommand> {
-  start: Coord;
-  command: C;
-  nativeDArgs: PathSegmentArgs<C> extends never ? number[] : PathSegmentArgs<C>;
+// PathDataCommon 中定义用于路径相关对象中的一些通用方法
+abstract class PathDataCommon extends DPathCommon {
+  constructor() {
+    super();
+  }
+  abstract start: Coord;
+  /**
+   * 生成路径字符串中的核心命令字符串
+   */
+  abstract getMiddleString(): string;
+
+  /**
+   * 生成最终的路径字符串
+   * @returns
+   */
+  protected getCompleteString() {
+    return `M ${this.start[0]} ${this.start[1]} ${this.getMiddleString()}`;
+  }
+}
+
+// PathSegment 对应组成路径的最小单位，除了 M 与 Z 之外只包含单个路径命令
+export class PathSegment<
+  C extends AbsoluteCommand = AbsoluteCommand,
+> extends PathDataCommon {
+  constructor(
+    public readonly start: Coord,
+    public readonly command: C,
+    public readonly args: PathSegmentArgs<C>,
+  ) {
+    super();
+  }
+
+  public get end(): Coord {
+    switch (this.command) {
+      case "L":
+      case "T":
+        return [this.args[0], this.args[1]] as Coord;
+      case "H":
+        return [this.args[0], this.start[1]] as Coord;
+      case "V":
+        return [this.start[0], this.args[0]] as Coord;
+      case "C":
+        return [this.args[4], this.args[5]] as Coord;
+      case "S":
+      case "Q":
+        return [this.args[2], this.args[3]] as Coord;
+      case "A":
+        return [this.args[5], this.args[6]] as Coord;
+      default:
+        throw new Error(`Invalid command: ${this.command}`);
+    }
+  }
+
+  public getMiddleString() {
+    return this.command + " " + this.args.join(" ");
+  }
+
+  public toString(option: { close: Boolean } = { close: true }) {
+    return this.getCompleteString() + (option.close ? " Z" : "");
+  }
+}
+
+export class PathData extends PathDataCommon {
+  private pathSegmentList: PathSegment[];
+
+  constructor(start: Coord, command: AbsoluteCommand, args: any) {
+    super();
+    this.pathSegmentList = [new PathSegment(start, command, args)];
+  }
+
+  public get start() {
+    return this.pathSegmentList[0].start;
+  }
+
+  public get end() {
+    return this.pathSegmentList[this.pathSegmentList.length - 1].end;
+  }
+
+  public addPath: AddPathOverloads = function (this: PathData, ...args: any[]) {
+    const createPathSegmentInfoArgs = [
+      ...this.end,
+      ...args,
+    ] as unknown as Parameters<CreatePathSegmentInfoOverloads>;
+    const { start, command, nativeDArgs } = this.createPathSegmentInfo(
+      ...createPathSegmentInfoArgs,
+    );
+    this.pathSegmentList.push(new PathSegment(start, command, nativeDArgs));
+    return this;
+  };
+
+  public getMiddleString() {
+    return this.pathSegmentList.reduce(
+      (accum, currPathSegment) =>
+        accum + (accum && " ") + currPathSegment.getMiddleString(),
+      "",
+    );
+  }
+
+  public toString(option: { close: Boolean } = { close: true }) {
+    return this.getCompleteString() + (option.close ? " Z" : "");
+  }
 }
 
 export default class DPath extends DPathCommon {
@@ -351,43 +240,6 @@ export default class DPath extends DPathCommon {
     this.pathSet.add(result);
     return result;
   };
-}
 
-class PathData extends DPathCommon {
-  private pathSegmentList: PathSegment[];
-
-  constructor(start: Coord, command: AbsoluteCommand, args: any) {
-    super();
-    this.pathSegmentList = [new PathSegment(start, command, args)];
-  }
-
-  public get start() {
-    return this.pathSegmentList[0].start;
-  }
-
-  public get end() {
-    return this.pathSegmentList[this.pathSegmentList.length - 1].end;
-  }
-
-  public addPath(this: PathData, ...args: any[]) {
-    const createPathSegmentInfoArgs = [
-      ...this.end,
-      ...args,
-    ] as unknown as Parameters<CreatePathSegmentInfoOverloads>;
-    const { start, command, nativeDArgs } = this.createPathSegmentInfo(
-      ...createPathSegmentInfoArgs,
-    );
-    this.pathSegmentList.push(new PathSegment(start, command, nativeDArgs));
-    return this;
-  }
-
-  public toString() {
-    let result = `M ${this.start[0]} ${this.start[1]}`;
-    for (const pathSegment of this.pathSegmentList) {
-      result += " " + pathSegment.getSegment();
-    }
-    result += " Z";
-    console.log(6666, result);
-    return result;
-  }
+  public connect() {}
 }
---
+++ src/d-path/types.ts
@@ -0,0 +1,168 @@
+import { PathSegment, PathData } from "./index";
+import type { TupleToIntersection, PrependParams } from "./utility-types";
+
+export type Coord = [number, number];
+
+export type RelativeCommand = "l" | "h" | "v" | "c" | "s" | "q" | "t" | "a";
+export type AbsoluteCommand = "L" | "H" | "V" | "C" | "S" | "Q" | "T" | "A";
+export type Command = RelativeCommand | AbsoluteCommand;
+export type ToAbsoluteCommand<Command> = Command extends "l" | "L"
+  ? "L"
+  : Command extends "h" | "H"
+    ? "H"
+    : Command extends "v" | "V"
+      ? "V"
+      : Command extends "c" | "C"
+        ? "C"
+        : Command extends "s" | "S"
+          ? "S"
+          : Command extends "q" | "Q"
+            ? "Q"
+            : Command extends "t" | "T"
+              ? "T"
+              : Command extends "a" | "A"
+                ? "A"
+                : never;
+
+// 用于 createPath 方法的重载签名组成的元祖类型，后续可以转为交叉类型来实现重载；其它方法有类似的重载签名也可以以这个元祖为基础映射类型以进行拓展
+type AddPathOverloadsTuple = [
+  <C extends "l" | "L">(command: C, endX: number, endY: number) => PathData,
+  <C extends "l" | "L">(command: C, end: Coord) => PathData,
+  <C extends "h" | "H">(command: C, endX: number) => PathData,
+  <C extends "v" | "V">(command: C, endY: number) => PathData,
+  <C extends "c" | "C">(
+    command: C,
+    controlPointX1: number,
+    controlPointY1: number,
+    controlPointX2: number,
+    controlPointY2: number,
+    endX: number,
+    endY: number,
+  ) => PathData,
+  <C extends "c" | "C">(
+    command: C,
+    controlPoint1: Coord,
+    controlPoint2: Coord,
+    end: Coord,
+  ) => PathData,
+  <C extends "s" | "S">(
+    command: C,
+    controlPointX: number,
+    controlPointY: number,
+    endX: number,
+    endY: number,
+  ) => PathData,
+  <C extends "s" | "S">(command: C, controlPoint: Coord, end: Coord) => any,
+  <C extends "q" | "Q">(
+    command: C,
+    controlPointX: number,
+    controlPointY: number,
+    endX: number,
+    endY: number,
+  ) => PathData,
+  <C extends "q" | "Q">(command: C, controlPoint: Coord, end: Coord) => any,
+  <C extends "t" | "T">(command: C, endX: number, endY: number) => any,
+  <C extends "t" | "T">(command: C, end: Coord) => any,
+  <C extends "a" | "A">(
+    command: C,
+    rx: number,
+    ry: number,
+    rotation: number,
+    largeArcFlag: boolean | 0 | 1,
+    sweepFlag: boolean | 0 | 1,
+    endX: number,
+    endY: number,
+  ) => PathData,
+  <C extends "a" | "A">(
+    command: C,
+    rx: number,
+    ry: number,
+    rotation: number,
+    largeArcFlag: boolean | 0 | 1,
+    sweepFlag: boolean | 0 | 1,
+    end: Coord,
+  ) => PathData,
+  <C extends "a" | "A">(
+    command: C,
+    radius: number | [number, number],
+    end: Coord,
+    rotation?: number,
+    largeArcFlag?: boolean | 0 | 1,
+    sweepFlag?: boolean | 0 | 1,
+  ) => PathData,
+];
+
+// 用于将 AddPathOverloadsTuple 转换为 CreatePathOverloadsTuple 的工厂类型，实质上是向 AddPathOverloadsTuple 中的每个函数签名的参数列表前添加一个 start: Coord 参数
+type CreatePathOverloadsTupleFactory<T extends ((...args: any[]) => any)[]> =
+  T extends [
+    infer First extends (...args: any) => any,
+    ...infer Rest extends ((...args: any) => any)[],
+  ]
+    ? [
+        PrependParams<First, [start: Coord]>,
+        ...CreatePathOverloadsTupleFactory<Rest>,
+      ]
+    : [];
+// 用于 addPath 方法的重载签名组成的元祖类型
+type CreatePathOverloadsTuple =
+  CreatePathOverloadsTupleFactory<AddPathOverloadsTuple>;
+
+// 把一个函数签名的返回值变成 pathSegmentInfo, I 指定了要选用的 Command 类型在该函数参数中的序号
+type MakePathSegmentInfoResult<
+  T extends (...args: any[]) => any,
+  I extends number,
+> = T extends (...args: any[]) => any
+  ? (
+      ...args: Parameters<T>
+    ) => pathSegmentInfo<ToAbsoluteCommand<Parameters<T>[I]>>
+  : never;
+
+// 把 CreatePathOverloadsTuple 中的函数签名转换为 CreatePathSegmentInfoOverloadsTuple 的工具函数，
+type CreatePathSegmentInfoOverloadsTupleFactory<
+  T extends ((...args: any[]) => any)[],
+> = T extends [
+  infer First extends (...args: any) => any,
+  ...infer Rest extends ((...args: any) => any)[],
+]
+  ? [
+      MakePathSegmentInfoResult<First, 1>,
+      ...CreatePathSegmentInfoOverloadsTupleFactory<Rest>,
+    ]
+  : [];
+
+// 用于 createPathSegmentInfo 方法的重载签名组成的元祖类型
+type CreatePathSegmentInfoOverloadsTuple =
+  CreatePathSegmentInfoOverloadsTupleFactory<CreatePathOverloadsTuple>;
+
+// 用于 createPath 方法的重载签名
+export type CreatePathOverloads = TupleToIntersection<CreatePathOverloadsTuple>;
+// 用于 addPath 方法的重载签名
+export type AddPathOverloads = TupleToIntersection<AddPathOverloadsTuple>;
+// 用于 CreatePathSegmentInfoOverloads 方法的重载签名
+export type CreatePathSegmentInfoOverloads =
+  TupleToIntersection<CreatePathSegmentInfoOverloadsTuple>;
+
+// 根据 SVG 命令名称返回对应命令参数组成的元祖类型
+export type PathSegmentArgs<C extends Command> = C extends "l" | "L"
+  ? [number, number]
+  : C extends "h" | "H"
+    ? [number]
+    : C extends "v" | "V"
+      ? [number]
+      : C extends "c" | "C"
+        ? [number, number, number, number, number, number]
+        : C extends "s" | "S"
+          ? [number, number, number, number]
+          : C extends "q" | "Q"
+            ? [number, number, number, number]
+            : C extends "t" | "T"
+              ? [number, number]
+              : C extends "a" | "A"
+                ? [number, number, number, 0 | 1, 0 | 1, number, number]
+                : never;
+
+export interface pathSegmentInfo<C extends AbsoluteCommand = AbsoluteCommand> {
+  start: Coord;
+  command: C;
+  nativeDArgs: PathSegmentArgs<C> extends never ? number[] : PathSegmentArgs<C>;
+}
